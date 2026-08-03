b1be66a9576e7b099260900abdb6977509ce0388
Anamarkelova <amarkelova@myyahoo.com>
2026-08-03 17:39:25 -0400

    Add Project1 expressions alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+// Display a message using the window.alert() method. 
+window.alert("Hello, World!");
+
+// Display a message using the document.write() method. 
+document.write("Hello, World!");
+
+// Create two variables containing parts of a sentence. 
+var Sent1 = "This is the beginning of the string"; 
+var Sent2 = " and this is the end of the string";
+
+// Concatenate Sent1 and Sent2 and display the complete sentence. 
+document.write(Sent1 + Sent2);
+
+// Write an expression that adds two numbers together. 
+var result = 10 + 5;
+
+// Display the result of the expression. 
+document.write("<br>The result of the expression is: " + result);
